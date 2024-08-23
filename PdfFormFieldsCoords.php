c551265b76b4cc371d3330d03b062110e07727fd
Edgar <edgar@zentralweb.de>
2024-08-23 12:30:38 +0300

    Upd

---
+++ PdfFormFieldsCoords.php
@@ -0,0 +1,44 @@
+<?php
+
+use Smalot\PdfParser\Element\ElementXRef;
+use Smalot\PdfParser\Parser;
+use Smalot\PdfParser\PDFObject;
+
+class PdfFormFieldsCoords
+{
+    public function getFieldsData($filePath)
+    {
+        $parser = new Parser();
+        $pdf = $parser->parseFile($filePath);
+        $pages = $pdf->getPages();
+        $mediaHeight = $pages[0]->getDetails()['MediaBox'][3];
+
+        $fieldsData = [];
+        foreach ($pages as $page) {
+            $annots = $page->getHeader()->getElements()['Annots'] ?? null;
+            if ($annots) {
+                $elements = is_array($annots->getContent()) ? $annots->getContent() : $annots->getHeader()->getElements();
+                foreach ($elements as $element) {
+                    $inputDetails = null;
+                    if (get_class($element) == PDFObject::class) {
+                        $inputDetails = $element->getHeader()->getDetails();
+                    } else if (get_class($element) == ElementXRef::class) {
+                        $elementNr = $element->getContent();
+                        $inputDetails = $page->getDocument()->getObjects()[$elementNr]->getHeader()->getDetails();
+                    }
+                    if ($inputDetails) {
+                        $name = !isset($inputDetails['T']) ? $inputDetails['Parent']['T'] : $inputDetails['T'];
+                        $coords = $inputDetails['Rect'];
+
+                        $fieldsData[$page->getPageNumber()][$name] = [
+                            'x' => $coords[0],
+                            'y' => $mediaHeight - $coords[3],
+                            'w' => $coords[2] - $coords[0],
+                            'h' => $coords[3] - $coords[1],
+                        ];
+                    }
+                }
+            }
+        }
+    }
+}
